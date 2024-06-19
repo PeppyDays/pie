b1abe5ac6aae01de7a496d56c46433e86297a9bf
Arine <peppydays@gmail.com>
2024-06-19 22:28:24 +0900

    Made Slack Event API url verification passed

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Pie
 
-..
+## Local Setup
+
+### Ngrok
+
+To check if Slack event subscription works well, you have to respond the challenge request described [url_verification](https://api.slack.com/events/url_verification). To do that, Ngrok is required to register an URL from local laptop.
+
+```bash
+brew install ngrok/ngrok/ngrok
+ngrok config add-authtoken <token>
+
+# This will create admin web interface to check the public URL to register in Slack API
+ngrok http 8080
+```
+
+### Slack Apps
+
+Go to `https://api.slack.com/apps` and navigate to the app you want to use. Go to `Event Subscriptions` and add the public URL from Ngrok.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{extract::Json, routing::post, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let app = Router::new().route("/events", post(handle_event_message));
+
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+}
+
+async fn handle_event_message(
+    Json(message): Json<serde_json::Value>,
+) -> axum::Json<serde_json::Value> {
+    println!("Received events: {:?}", message);
+
+    match message.get("challenge") {
+        Some(challenge) => axum::Json(serde_json::json!({"challenge": challenge})),
+        _ => axum::Json(serde_json::json!({})),
+    }
 }
